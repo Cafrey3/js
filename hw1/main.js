7844f5d89a01e16171291a3958caed2b8fda002b
Cafrey3 <tokar9980@gmail.com>
2021-11-11 02:39:59 +0200

    hw

---
+++ hw1/main.js
@@ -0,0 +1,241 @@
+
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Ім'ям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+//     5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+//
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
+
+
+
+
+
+let hello = "hello";
+console.log(hello);
+document.write(hello);
+alert(hello);
+
+hello = "bye";
+console.log(hello);
+document.write(hello);
+alert(hello)
+
+let owu = "owu";
+console.log(owu);
+document.write(owu);
+alert(owu);
+
+owu = "not owu";
+console.log(owu);
+document.write(owu);
+alert(owu);
+
+let com = "com";
+console.log(com);
+document.write(com);
+alert(com);
+
+com = "org";
+console.log(com);
+document.write(com);
+alert(com);
+
+let ua = "ua";
+console.log(ua);
+document.write(ua);
+alert(ua);
+
+ua = "gr";
+console.log(ua);
+document.write(ua);
+alert(ua);
+
+let one = "1";
+console.log(one);
+document.write(one);
+alert(one);
+
+one = "-1";
+console.log(one);
+document.write(one);
+alert(one);
+
+let ten = "10";
+console.log(ten);
+document.write(ten);
+alert(ten);
+
+ten = "20";
+console.log(ten);
+document.write(ten);
+alert(ten);
+
+let minusnine = "-999";
+console.log(minusnine);
+document.write(minusnine);
+alert(minusnine);
+
+minusnine = "-666";
+console.log(minusnine);
+document.write(minusnine);
+alert(minusnine);
+
+let onetwothree = "123";
+console.log(onetwothree);
+document.write(onetwothree);
+alert(onetwothree);
+
+onetwothree = "321";
+console.log(onetwothree);
+document.write(onetwothree);
+alert(onetwothree);
+
+let pi = "3.14";
+console.log(pi);
+document.write(pi);
+alert(pi);
+
+pi = "6.18";
+console.log(pi);
+document.write(pi);
+alert(pi);
+
+let twodotseven = "2.7";
+console.log(twodotseven);
+document.write(twodotseven);
+alert(twodotseven);
+
+twodotseven = "88";
+console.log(twodotseven);
+document.write(twodotseven);
+alert(twodotseven);
+
+let sixteen = "16";
+console.log(sixteen);
+document.write(sixteen);
+alert(sixteen);
+
+sixteen = "69";
+console.log(sixteen);
+document.write(sixteen);
+alert(sixteen);
+
+let tru = "true";
+console.log(tru);
+document.write(tru);
+alert(tru);
+
+tru = "good";
+console.log(tru);
+document.write(tru);
+alert(tru);
+
+let lie = "false";
+console.log(lie);
+document.write(lie);
+alert(lie);
+
+lie = "bad";
+console.log(lie);
+document.write(lie);
+alert(lie);
+
+
+
+
+
+
+
+// let firstName = 'Roman';
+// let lastName = 'Tokarchuk';
+// let middleName = 'Mykolayovych';
+// let person = firstName+ lastName+ middleName
+// console.log(person)
+// document.write(person)
+
+
+
+// let firstName = prompt("Roman");
+// let middleName = prompt("Mykolayovych");
+// let age = prompt("23");
+// alert("Вітаю Роман Миколайович. Тобі 23 роки");
+
+
+
+
+// let a = 100;
+// console.log(typeof (100))
+//
+// // let b = '100';
+// console.log(typeof ('100'))
+//
+// // let c = true;
+// console.log(typeof (true))
+
+
+
+
+// // 5 ? 6 -> true
+// console.log(5<6)
+// // 5 ? 6 -> false
+// console.log(5>6)
+// // 5 ? 6 -> false
+// console.log(5==6)
+// // 5 ? 6 -> false
+// console.log(5>=6)
+// // 10 ? 10 -> true
+// console.log(10==10)
+// // 10 ? 10 -> true
+// console.log(10===10)
+// // 10 ? 10 -> false
+// console.log(10>10)
+// // 10 ? 10 -> false
+// console.log(10<10)
+// // 10 ? 10 -> false
+// console.log(10!==10)
+// // 123 ? '123' -> false
+// console.log(123==='123')
+// // 123 ? '123' -> true
+// console.log(123=='123')
+
+
+
+
+// document.write(str + a + "<br/>"); // тут ми просто добавили біля двадця'тки 5 і буде 205
+// document.write(str - a + "<br/>"); // тут просто мінус 5 і буде 15
+// document.write(str * "2" + "<br/>"); // а тут 40, тому що помножили 20 на 2
+// document.write(str / 2 + "<br/>"); // тут ми поділили на два 20 і буде 10
+
+
+
+
+
+
+
+
+
+
