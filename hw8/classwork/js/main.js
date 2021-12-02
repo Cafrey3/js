b8a764ccc5972c36878c0881abb1ab06522bed27
Cafrey3 <tokar9980@gmail.com>
2021-12-03 00:19:51 +0200

    hw8

--- hw8/classwork/js/main.js
+++ hw8/classwork/js/main.js
@@ -7,8 +7,7 @@ mainHeader.classList.add('sept_2021');
 
 let ulList = document.getElementsByTagName('ul');
 for (const item of ulList) {
-    item.style.width='400px'
-    item.style.backgroundColor = 'yellow';
+    item.style.width = '400px'
 }
 
 // c) робить ширину всіх елементів з класом linkList шириною 50%
@@ -20,17 +19,71 @@ for (const item of liWidth) {
 
 // d) отримує текст який зберігається в елементі з класом listElement2
 
-let text = document.getElementsByClassName('listElement2').innerText;
-// console.log(text)
-
-
+let textlink = document.getElementsByClassName('listElement2')[0].innerText;
+console.log(textlink);
 
 // e) отримує всі елементи li та змінює ім колір фону на сірий
+
+let backgroundLi = document.getElementsByTagName('ul');
+for (const item of backgroundLi) {
+    item.style.backgroundColor = 'grey';
+}
+
 // f) отримує всі елементи 'a' та додає їм клас anchor
-// g) отримує всі елементи 'a' та у випадку, якщо текстовий контен елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+
+// let aElements = document.getElementsByTagName('a');
+// for (const item of aElements) {
+//     item.classList.add('anchor');
+// }
+
+// g) отримує всі елементи 'a' та у випадку, якщо текстовий контент елементу дорівнює link3, змінює йому розмір тексту на 40 пікселів
+
+let aFontSize = document.getElementsByTagName('a');
+for (const item of aFontSize) {
+    if (item.innerText === 'link3') {
+        item.style.fontSize = '40px';
+    }
+}
+
 // h) отримує всі елементи 'a' та додає їм клас element_XXX. Де XXX - текстовий контент елементу a
+
+let aAddClass = document.getElementsByTagName('a');
+for (const item of aAddClass) {
+    item.classList.add(`element_${item.innerText}`);
+}
+
 // i) отримує всі елементи 'sub-header' та змінює колір фону. Фон отримати з prompt()
-// j) отримує всі елементи 'sub-header' та змінює колір тексту у видаку якщо текст елемнту = content 2 segment . Колір отримати з prompt()
+
+// let h3Title = document.getElementsByClassName('sub-header');
+// for (const title of h3Title) {
+//     title.style.backgroundColor = prompt('Enter your color for first and second title:');
+// }
+
+// j) отримує всі елементи 'sub-header' та змінює колір тексту у випадку якщо текст елементу = content 2 segment . Колір отримати з prompt()
+
+// let subHeaderElements = document.getElementsByClassName('sub-header');
+// for (const title of subHeaderElements) {
+//     if (title.innerText === 'content 2 segment') {
+//         title.style.color = prompt('Enter color for title:');
+//     }
+// }
+
+
 // k) отримує елемент з класом content_1 та заміняє  в ньому тест на довільний. Текст отримати з prompt()
+
+// let elemText = document.getElementsByClassName('content_1');
+// for (const item of elemText) {
+//     item.innerText = prompt('Type text here:');
+// }
+
 // l) отримати елементи p та змінити їм padding на 20px
-// m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year. Пример sep-2021)
+
+let pList = document.getElementsByTagName('p');
+for (const p of pList) {
+    p.style.padding = '20px';
+}
+
+// m) отримати елементи з класом text2 та змінити їм текст на назву групи (mon-year. Приклад sep-2021)
+
+let changedText = document.getElementsByClassName('text2')[0];
+changedText.innerText = 'September - 2021';
